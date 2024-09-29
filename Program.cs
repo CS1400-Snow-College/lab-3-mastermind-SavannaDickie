98bbbf79f48013bb714cc161aad7d8d174eaf6d0
Savanna Dickie <personal3@Savannas-MacBook-Pro.local>
2024-09-29 15:04:12 -0600

    greeting

--- Program.cs
+++ Program.cs
@@ -2,3 +2,11 @@
 // Name: Savanna Dickie 
 // Date: 9/29/24
 // Lab: Lab 3 - Mastermind
+using Microsoft.Win32.SafeHandles;
+
+
+string secret = "ba";
+int maxLetter = 2;
+int guessMax = 3;
+bool guess = false; 
+Console.WriteLine($"I have chosen {maxLetter} letters between 'a' and 'b' and have arranged them in a paticular order. Your job is to guess the letters and put them in the write order.");
